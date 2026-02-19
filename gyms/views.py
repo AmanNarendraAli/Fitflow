81938bce8aedc13592965ab961d7c6aac9d26964
AmanNarendraAli <amannarendraali@gmail.com>
2026-02-19 13:44:40 +0530

    added basic frontend

--- gyms/views.py
+++ gyms/views.py
@@ -10,13 +10,13 @@ from django.utils.decorators import method_decorator
 @method_decorator(role_required(['OWNER', 'STAFF']), name='dispatch')
 class RoomListView(GymQuerySetMixin, ListView):
     model = Room
-    template_name = 'gyms/room_list.html'
+    template_name = 'room_list.html'
     context_object_name = 'rooms'
 
 @method_decorator(role_required(['OWNER', 'STAFF']), name='dispatch')
 class RoomCreateView(GymQuerySetMixin, CreateView):
     form_class = RoomForm
-    template_name = 'gyms/room_form.html'
+    template_name = 'room_form.html'
     success_url = reverse_lazy('room_list')
     def form_valid(self, form):
         form.instance.gym = self.request.user.gym #sets gym in form to user's signed-in gym
